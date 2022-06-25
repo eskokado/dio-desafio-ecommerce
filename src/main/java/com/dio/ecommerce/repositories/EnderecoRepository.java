25f82f04defd4ca1058aafd31cbb70ada0cb6a65
eskokado <eskokado@gmail.com>
2022-06-25 12:00:24 -0300

    feat: repositories

---
+++ src/main/java/com/dio/ecommerce/repositories/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.dio.ecommerce.repositories;
+
+import com.dio.ecommerce.models.Endereco;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
